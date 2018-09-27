08883c1683426755fe23b23dd8ff9fee132e88ab
Le Bao Trung <trunglbth1809003@fpt.edu.vn>
2018-09-28 06:01:23 +0700

    cong tru nhan chia

--- main.c
+++ main.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-
-// khai báo và định nghĩa hàm
-int conghaiso(int x, int y) {
-    return x + y;
-}
-
-int truhaiso(int x, int y) {
-    return x - y;
-}
-
+int a, b, ketqua, luachon;
 int main() {
-    int a, b, ketqua, luachon;
+
     printf("vui long nhap so a: \n");
     scanf("%d", &a);
     printf("vui long nhap so b: \n");
     scanf("%d", &b);
     printf("lua chon phep tinh:\n");
     printf("1. phep cong\n");
-    printf("2. php tru\n");
-    printf("nhap lua chon cua ban (1|2 \n)");
+    printf("2. phep tru\n");
+    printf("3. phep nhan\n");
+    printf("4. phep chia\n");
+    printf("nhap lua chon cua ban (1|2|3|4\n)");
     scanf("%d", &luachon);
-    //
     if (luachon == 1) {
         printf("chon phep cong\n");
         printf("ket qua: %d + %d =%d", a, b, a + b);
     } else if (luachon == 2) {
         printf("chon phep tru\n");
         printf("ket qua: %d - %d =%d", a, b, a - b);
+    } else if (luachon == 3) {
+        printf("chon phep nhan\n");
+        printf("ket qua: %d * %d = %d", a, b, a * b);
+    } else if (luachon == 4) {
+        printf("chon phep chia\n");
+        printf("ket qua: %d / %d = %0.3f", a, b, (float) a / b);
     } else {
         printf("lua chon sai");
     }
-    printf("%d", ketqua);
 
 
     return 0;
