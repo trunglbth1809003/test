89f0969ed90019904443facc5faf838303f8b68d
Le Bao Trung <trunglbth1809003@fpt.edu.vn>
2018-09-28 18:37:26 +0700

    thay chua bai tap

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
-#include <stdio.h>
-int a, b, ketqua, luachon;
-int main() {
+#include "stdio.h"
+#include <stdlib.h>
 
-    printf("vui long nhap so a: \n");
-    scanf("%d", &a);
-    printf("vui long nhap so b: \n");
-    scanf("%d", &b);
+int a, b, luachon;
+
+int phepcong(int a, int b) {
+    return a + b;
+}
+
+int pheptru(int a, int b) {
+    return a - b;
+}
+
+int phepnhan(int a, int b) {
+    return a * b;
+}
+
+float phepchia(int q, int b) {
+    if (b == 0) {
+        printf(" khong the chia cho 0");
+        exit(1);
+    }
+    return (float) a / b;
+}
+void taomenu() {
     printf("lua chon phep tinh:\n");
     printf("1. phep cong\n");
     printf("2. phep tru\n");
     printf("3. phep nhan\n");
     printf("4. phep chia\n");
-    printf("nhap lua chon cua ban (1|2|3|4\n)");
+}
+int main() {
+
+    printf("vui long nhap so a: ");
+    scanf("%d", &a);
+    printf("vui long nhap so b: ");
+    scanf("%d", &b);
+    taomenu();
+
+    printf("nhap lua chon cua ban (1|2|3|4)\n");
     scanf("%d", &luachon);
     if (luachon == 1) {
         printf("chon phep cong\n");
@@ -24,11 +50,10 @@ int main() {
         printf("ket qua: %d * %d = %d", a, b, a * b);
     } else if (luachon == 4) {
         printf("chon phep chia\n");
-        printf("ket qua: %d / %d = %0.3f", a, b, (float) a / b);
+        printf("ket qua: %d / %d = %.4f", a, b, phepchia(a, b));
     } else {
         printf("lua chon sai");
     }
-
 
     return 0;
 }
